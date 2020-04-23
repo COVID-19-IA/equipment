0dcaab7d3d44fff1e90f018ceabec7485fe68cf5
oti <otiext@gmail.com>
2020-04-23 19:38:22 +0900

    follow から開発環境をクローンした

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const dayjs = require("dayjs");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/**/*.(png|jpg|svg|webp|pdf)");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  eleventyConfig.addWatchTarget("./src/");
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateFormat", (v) => {
+    return dayjs(v).format("YYYY年MM月DD日 HH:mm");
+  });
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateISOFormat", (v) => {
+    return dayjs(v).format("YYYY-MM-DDTHH:mmZ");
+  });
+
+  // Nunjucks Shortcode
+  eleventyConfig.addNunjucksShortcode("baseTag", (v) => {
+    return process.env.NODE_ENV === "production" ? `<base href="${v}">` : "";
+  });
+
+  eleventyConfig.addNunjucksShortcode("unsslize", (v) => {
+    return v.replace("https:", "http:");
+  });
+
+  return {
+    dir: { input: "src", output: "docs" },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md", "html", "yml"],
+    htmlTemplateEngine: "njk",
+  };
+};
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # equipment
+
+https://covid-19-act.jp/equipment/
+
+## development
+
+Using Node.js v12.16.1
+
+```bash
+npm i -g yarn
+yarn install
+yarn dev
+```
